ca4641b34a5786ac64a3f4dea5156f6c2dd41a82
Gerard Braad <me@gbraad.nl>
2023-06-14 10:34:09 +0000

    Initial publish

---
+++ precache-manifest.077267e34a91c3757ed6409cd33a5e8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96fba6cdb0ee78c720584c518160ebe",
+    "url": "/gbraad/magic-deckshuffle/index.html"
+  },
+  {
+    "revision": "ac338101c2a1210ab64f",
+    "url": "/gbraad/magic-deckshuffle/static/css/main.731ce8ce.chunk.css"
+  },
+  {
+    "revision": "42be9ac8b94ec00ba4d6",
+    "url": "/gbraad/magic-deckshuffle/static/js/2.8ad6fd9a.chunk.js"
+  },
+  {
+    "revision": "ac338101c2a1210ab64f",
+    "url": "/gbraad/magic-deckshuffle/static/js/main.8a2dcd1f.chunk.js"
+  },
+  {
+    "revision": "1303151418abe0d0da0e",
+    "url": "/gbraad/magic-deckshuffle/static/js/runtime~main.4c8d58a7.js"
+  }
+]);
